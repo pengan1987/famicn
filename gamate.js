0d79dd415bba3e93ad0c518904d50aa3ea74ab52
Andy Zhou <pengan1987@msn.com>
2021-01-24 01:15:19 +0800

    Add Bit-Corp Gamate emulator and games

---
+++ gamate.js
@@ -0,0 +1,80 @@
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function runMAME(cart,game) {
+    var wantsWASM = 'WebAssembly' in window;
+   
+    var wasmjs_filename = "https://dnbwg.cdn.bcebos.com/emularity-common/emulators/jsmess/mamegamate_wasm.js";
+    var wasm_filename = "https://dnbwg.cdn.bcebos.com/emularity-common/emulators/jsmess/mamegamate_wasm.wasm"
+    var js_filename = "https://dnbwg.cdn.bcebos.com/emularity-common/emulators/jsmess/mamegamate.js";
+    var bios = "https://dnbwg.cdn.bcebos.com/emularity-common/bios/gamate.zip"
+
+    var emulator = new Emulator(document.querySelector(".emucanvas"),
+        postRun,
+        new JSMESSLoader(JSMESSLoader.driver("gamate"),
+            JSMESSLoader.nativeResolution(480, 450),
+            JSMESSLoader.emulatorJS(wantsWASM ? wasmjs_filename : js_filename),
+            JSMESSLoader.emulatorWASM(wantsWASM && wasm_filename),
+            JSMESSLoader.mountFile("gamate.zip",
+                JSMESSLoader.fetchFile("BIOS",
+                    bios)),
+            JSMESSLoader.mountFile(game + ".zip",
+                JSMESSLoader.fetchFile("Game File",
+                    cart)),
+            JSMAMELoader.peripheral("cart", game + ".zip")
+        ));
+    emulator.start({ waitAfterDownloading: true });
+}
+
+function postRun() {
+    console.log("Emulator started");
+    var bodyWidth = $("body").width();
+    if (bodyWidth < 600) {
+        $("#mobile-tools").show();
+        resizeCanvas();
+    }
+}
+
+function resizeCanvas() {
+    var bodyWidth = $("body").width();
+    var canvasWidth = $("#canvas").width();
+    var canvasHeight = $("#canvas").height();
+    if (bodyWidth < canvasWidth) {
+        //Resize canvas for mobile device
+        var newHeight = Math.round(canvasHeight * bodyWidth / canvasWidth);
+        $("#canvas").width(bodyWidth);
+        $("#canvas").height(newHeight);
+    }
+}
+
+//IE 11 string includes ployfill
+if (!String.prototype.includes) {
+    Object.defineProperty(String.prototype, 'includes', {
+        value: function (search, start) {
+            if (typeof start !== 'number') {
+                start = 0
+            }
+
+            if (start + search.length > this.length) {
+                return false
+            } else {
+                return this.indexOf(search, start) !== -1
+            }
+        }
+    })
+}
+
+$(document).ready(function () {
+    console.log("ready!");
+    var gameBaseUrl = "https://famicn-1255835060.file.myqcloud.com/gamate-roms/"
+    var game = getUrlVars()["game"];
+    var cart = gameBaseUrl + game + ".zip"
+
+    runMAME(cart, game);
+});
---
+++ gamate_list.js
@@ -0,0 +1,125 @@
+var machineList;
+var newMachineList = [];
+var pages = [];
+var loadedPage = 0;
+var platform = "gamate.html";
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function processJson(data) {
+    machineList = data;
+    reorderedList = splitArrayByTime(machineList);
+
+    var i, j, temparray, chunk = 32;
+    for (i = 0, j = reorderedList.length; i < j; i += chunk) {
+        temparray = reorderedList.slice(i, i + chunk);
+        pages.push(temparray);
+    }
+    showMachines(pages[loadedPage]);
+}
+
+function loadmore() {
+    showMachines(pages[loadedPage]);
+}
+
+function showMachines(machines) {
+    var base = $("#base-cell");
+    var machineListContainer = $("#machine-list");
+    for (i = 0; i < machines.length; i++) {
+        var machine = machines[i];
+        var clone = base.clone();
+        var title = machine.name + " ("+machine.year+") - " + machine.vendor;
+        var playerlink = platform + "?game=" + encodeURI(machine.id);
+
+        var imagePathNew = "http://famicn-1255835060.file.myqcloud.com/gamate-images";
+        var imageLink = "gamate_card_blank.jpg";
+        if (machine.image) {
+            imageLink = machine.image;
+            imageLink = imageLink.replace("{{image-path-new}}", imagePathNew);
+        }
+        if (machine.device) {
+            playerlink = playerlink + "&device=" + machine.device;
+        }
+
+        clone.show();
+        clone.attr("id", machine.id);
+        clone.find("a").attr("href", playerlink);
+        clone.find("figcaption").text(title);
+        clone.find(".figure-img").attr("src", imageLink);
+        clone.addClass("show-data");
+        machineListContainer.append(clone);
+    }
+    loadedPage++;
+    if (loadedPage >= pages.length) {
+        $("#loadmore").hide();
+    }
+}
+
+function search() {
+
+    var keyword = $("#search-text").val();
+    if (!keyword || keyword.length == 0) {
+        $(".show-data").remove();
+        loadedPage = 0;
+        processJson(machineList);
+        return;
+    }
+    var searchResult = [];
+    for (i = 0; i < machineList.length; i++) {
+        var text = machineList[i].name + machineList[i].vendor;
+        if (text.includes(keyword)) {
+            searchResult.push(machineList[i]);
+        }
+    }
+    pages = [];
+    loadedPage = 0;
+    $(".show-data").remove();
+    showMachines(searchResult);
+}
+
+function splitArrayByTime(someArray) {
+    var date = new Date();
+    var timestamp = date.getTime();
+    var timetoken = Math.round(timestamp / (600 * 1000));
+    var position = timetoken % someArray.length;
+    console.log(position);
+    var first = someArray.slice(0, position);
+    var second = someArray.slice(position);
+    return second.concat(first);
+}
+
+$(document).ready(function () { 
+    $.getJSON("gamate.json", processJson);
+});
+
+$(window).scroll(function () {
+    if ($(document).height() - $(window).height() - $(window).scrollTop() < 50 && loadedPage < pages.length) {
+        showMachines(pages[loadedPage]);
+    }
+});
+
+function testImage() {
+    for (var i = 0; i < machineList.length; i++) {
+        var machine = machineList[i];
+        $.ajax({
+            url: 'images/' + machine.filename + ".gif",
+            type: 'HEAD',
+            async: false,
+
+            error: function () {
+                //do something depressing
+                newMachineList.push(machine);
+            },
+            success: function () {
+                machine.image = 'images/' + machine.filename + ".gif";
+                newMachineList.push(machine);
+            }
+        });
+    }
+}
