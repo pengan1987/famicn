18cc8d4156dc7ea583e57b9130e58e2bddcebecc
Andy(Pengan) Zhou <pengan1987@msn.com>
2026-02-11 13:06:05 +0800

    remove baidu stats, change to releative path

--- baidustat.js
+++
@@ -1,8 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?0866d61c87dd26a57f5d7d55efb8c843";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
